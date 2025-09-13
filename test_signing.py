a2a228fc80617a82c0502850356a574bab0128af
Calvin Green <calvingreen94@aol.com>
2025-09-13 01:09:48 -0400

    Update test_signing.py

--- test_signing.py
+++ test_signing.py
@@ -3,7 +3,7 @@ from pqc.crypto_utils import generate_pqc_keys, sign_message, verify_signature
 
 def test_fake_signed_message(ip_address):
     public_key, private_key = generate_pqc_keys()
-    message = f"ALERT: Port scan detected on 192.168.1.157"
+    message = f"ALERT: Port scan detected on 0.0.0.0"
 
     signature = sign_message(private_key, message)
     verified = verify_signature(public_key, message, signature)
